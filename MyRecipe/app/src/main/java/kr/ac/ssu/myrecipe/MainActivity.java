00754f47bb1ecfad564855406e0483c11c88b53d
Frics <jimmy9711@naver.com>
2020-08-04 21:57:36 +0900

    Add FAB function

--- MyRecipe/app/src/main/java/kr/ac/ssu/myrecipe/MainActivity.java
+++ MyRecipe/app/src/main/java/kr/ac/ssu/myrecipe/MainActivity.java
@@ -1,6 +1,7 @@
 package kr.ac.ssu.myrecipe;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -9,6 +10,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +20,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // FAB 기본 세팅 (수정필요)
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "카메라", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
