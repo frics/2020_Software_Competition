85f8339f78b03a61d9b2d47a5b9c7cfc514a2e34
Frics <jimmy9711@naver.com>
2020-08-05 17:29:26 +0900

    카메라 액티비티 기능 완성 및 틴트 프레임 적용(마이너 버그 수정)

--- MyRecipe/app/src/main/java/kr/ac/ssu/myrecipe/Camera/CameraActivity.java
+++ MyRecipe/app/src/main/java/kr/ac/ssu/myrecipe/Camera/CameraActivity.java
@@ -1,26 +1,21 @@
 package kr.ac.ssu.myrecipe.Camera;
 
 
-import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.Manifest;
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
-
-import java.security.Permission;
 import java.util.List;
 
-import kr.ac.ssu.myrecipe.MainActivity;
 import kr.ac.ssu.myrecipe.R;
+
+import static android.Manifest.*;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -30,21 +25,26 @@ public class CameraActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_camera);
 
+
+
+
         TedPermission.with(this)
                 .setPermissionListener(permissionListener)
                 .setDeniedMessage("영수증 인식을 위해 권한이 필요합니다.\n")
-                .setPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .setPermissions(Manifest.permission.CAMERA)
+                //.setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 .check();
-        //사진 촬영 버튼
-        Button capture = findViewById(R.id.dummy_button);
-        capture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "찰칵", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        //카메라 프리뷰를 프래그먼트로 넘겨서 뷰 생성
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, CameraFragment.newInstance())
+                    .commit();
+        }
     }
+
+
+
 
 
     PermissionListener permissionListener = new PermissionListener() {
