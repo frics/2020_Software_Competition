e8bdde37c1390b981291983c420919c8ab5717c7
donggyuKim <ptsaturn68@daum.net>
2020-08-06 15:03:52 +0900

    Add recipe list

--- MyRecipe/app/src/main/java/kr/ac/ssu/myrecipe/ui/HomeFragment.java
+++ MyRecipe/app/src/main/java/kr/ac/ssu/myrecipe/ui/HomeFragment.java
@@ -4,16 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+
+import com.bumptech.glide.Glide;
 
 import kr.ac.ssu.myrecipe.R;
+import kr.ac.ssu.myrecipe.adapter.CustomAdapter;
+import kr.ac.ssu.myrecipe.loader.ImageLoadTask;
 
 public class HomeFragment extends Fragment {
+
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        /* URL 로딩 관련 코드 였던것..
+        ImageView imageview = (ImageView)view.findViewById(R.id.test);
+        String url = "http://file.okdab.com/UserFiles/searching/recipe/065300.jpg";
+        Glide.with(this).load(url)
+                .error(R.drawable.basic)
+                .into(imageview);
+*/
+
+        ViewPager pager = (ViewPager)view.findViewById(R.id.pager);
+        CustomAdapter adapter = new CustomAdapter(getLayoutInflater());
+
+        pager.setAdapter(adapter);
+
+
+
+        return view;
     }
 }
+
