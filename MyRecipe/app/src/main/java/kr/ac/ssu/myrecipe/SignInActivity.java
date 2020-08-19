6332058020e78f745bf9fff2b12a7cbbe7d10b98
Frics <jimmy9711@naver.com>
2020-08-19 17:51:18 +0900

    SignInActiviy layout 기본 구조 설정

---
+++ MyRecipe/app/src/main/java/kr/ac/ssu/myrecipe/SignInActivity.java
@@ -0,0 +1,19 @@
+package kr.ac.ssu.myrecipe;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+
+    }
+}
