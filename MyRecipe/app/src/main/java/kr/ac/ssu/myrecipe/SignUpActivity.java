a467b113fc7626574b84529dd4bd64d268f24521
Frics <jimmy9711@naver.com>
2020-08-20 23:19:37 +0900

    로그인 & 회원가입 레이아웃 구성 완료
    기능 구현만 완성하면 됨

---
+++ MyRecipe/app/src/main/java/kr/ac/ssu/myrecipe/SignUpActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.ssu.myrecipe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
