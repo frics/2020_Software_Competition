fb228ba8b45574ca4242d4d644e2cd6cf88f8618
Frics <jimmy9711@naver.com>
2020-08-31 17:37:40 +0900

    불필요 코드 삭제

--- MyRecipe/app/src/main/java/kr/ac/ssu/myrecipe/Camera/Crop/CropActivity.java
+++ MyRecipe/app/src/main/java/kr/ac/ssu/myrecipe/Camera/Crop/CropActivity.java
@@ -8,12 +8,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
-
 import com.theartofdev.edmodo.cropper.CropImage;
-
 import kr.ac.ssu.myrecipe.R;
 
 public class CropActivity extends AppCompatActivity {
@@ -37,7 +34,7 @@ public class CropActivity extends AppCompatActivity {
         Log.e("순서", "3");
 
         if (savedInstanceState == null) {
-            setMainFragmentByPreset(CropDemoPreset.RECT);
+            setMainFragmentByPreset();
         }
     }
 
@@ -102,11 +99,11 @@ public class CropActivity extends AppCompatActivity {
     }
 
 
-    private void setMainFragmentByPreset(CropDemoPreset demoPreset) {
+    private void setMainFragmentByPreset() {
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager
                 .beginTransaction()
-                .replace(R.id.container, CropFragment.newInstance(demoPreset))
+                .replace(R.id.container, CropFragment.newInstance())
                 .commit();
     }
 }
