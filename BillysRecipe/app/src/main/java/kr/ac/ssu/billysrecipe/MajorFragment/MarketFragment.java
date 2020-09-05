d03e5be8f58dcff562403ee7b0b741216d561a9b
donggyuKim <ptsaturn68@daum.net>
2020-09-05 23:02:31 +0900

    오늘 뭐묵지 하다가 감..

--- BillysRecipe/app/src/main/java/kr/ac/ssu/billysrecipe/MajorFragment/MarketFragment.java
+++ BillysRecipe/app/src/main/java/kr/ac/ssu/billysrecipe/MajorFragment/MarketFragment.java
@@ -4,20 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 import kr.ac.ssu.billysrecipe.R;
+import kr.ac.ssu.billysrecipe.adapter.TodayGridAdapter;
+import kr.ac.ssu.billysrecipe.recipe.Recipe;
 
 public class MarketFragment extends Fragment {
+
+    private GridView gridView;
+    private TodayGridAdapter todayGridAdapter;
 
     public static MarketFragment newInstance() {
         return new MarketFragment();
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_market, container, false);
+        View view = inflater.inflate(R.layout.fragment_market, container, false);
+
+        ArrayList<String> datalist = new ArrayList<>();
+        for(int i =0;i<9;i++)
+            datalist.add(Recipe.recipeList[i].image_url);
+
+        todayGridAdapter = new TodayGridAdapter(getContext(), R.layout.today_row, datalist);
+        gridView = view.findViewById(R.id.deck);
+        gridView.setAdapter(todayGridAdapter);
+
+
+
+
+
+        return view;
     }
 }
