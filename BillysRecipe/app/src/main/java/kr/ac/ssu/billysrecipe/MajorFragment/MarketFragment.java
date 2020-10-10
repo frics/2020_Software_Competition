e0b7bd9ae7831c1f4d2b28149c9783579d3d2515
유찬양 <clsrnthd7@gmail.com>
2020-10-10 14:23:46 +0900

    Week  recipe fragment

--- BillysRecipe/app/src/main/java/kr/ac/ssu/billysrecipe/MajorFragment/MarketFragment.java
+++ BillysRecipe/app/src/main/java/kr/ac/ssu/billysrecipe/MajorFragment/MarketFragment.java
@@ -1,45 +1,119 @@
 package kr.ac.ssu.billysrecipe.MajorFragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import kr.ac.ssu.billysrecipe.R;
+import kr.ac.ssu.billysrecipe.RefrigerRatorDB.RefrigeratorData;
+import kr.ac.ssu.billysrecipe.adapter.CategoryAdapter;
+import kr.ac.ssu.billysrecipe.adapter.DietListAdapter;
+import kr.ac.ssu.billysrecipe.adapter.RefrigeratorAdapter;
 import kr.ac.ssu.billysrecipe.adapter.TodayGridAdapter;
 import kr.ac.ssu.billysrecipe.recipe.Recipe;
+import kr.ac.ssu.billysrecipe.recipe.RecipeOrderList;
 
 public class MarketFragment extends Fragment {
-
-    private GridView gridView;
-    private TodayGridAdapter todayGridAdapter;
-
-    public static MarketFragment newInstance() {
-        return new MarketFragment();
-    }
+    private ArrayList<ArrayList<DietListAdapter.Data>> dataList;
+    private ArrayList<String> weeklist;
+    private RecyclerView weekRecycler;
+    private RecyclerView dietRecycler;
+    private DietListAdapter dietListAdapter;
+    private CategoryAdapter weekAdapter;
+    private ProgressBar progressBar;
+    private TextView totalCalories;
+    private TextView percentage;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_market, container, false);
+        weekRecycler = (RecyclerView)view.findViewById(R.id.diet_week);
+        dietRecycler = (RecyclerView)view.findViewById(R.id.diet_list);
+        progressBar = (ProgressBar)view.findViewById(R.id.diet_progressBar);
+        totalCalories = (TextView)view.findViewById(R.id.diet_total_kcal);
+        percentage = (TextView)view.findViewById(R.id.diet_percent);
 
-        ArrayList<String> datalist = new ArrayList<>();
-        for(int i =0;i<9;i++)
-            datalist.add(Recipe.recipeList[i].image_url);
-
-        todayGridAdapter = new TodayGridAdapter(getContext(), R.layout.today_row, datalist);
-        gridView = view.findViewById(R.id.deck);
-        gridView.setAdapter(todayGridAdapter);
-
-
-
-
-
+        weekRecycler.setLayoutManager(new LinearLayoutManager(this.getContext(),LinearLayoutManager.HORIZONTAL,false));
+        dietRecycler.setLayoutManager(new LinearLayoutManager(this.getContext(),LinearLayoutManager.VERTICAL,false));
         return view;
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        setData();
+    }
+    public void setData(){
+        weeklist = new ArrayList<>();
+        weeklist.add("  일  ");
+        weeklist.add("  월  ");
+        weeklist.add("  화  ");
+        weeklist.add("  수  ");
+        weeklist.add("  목  ");
+        weeklist.add("  금  ");
+        weeklist.add("  토  ");
+
+        dataList = new ArrayList<>();
+        //데이터 추가
+        dataList.add(new ArrayList<DietListAdapter.Data>());
+        dataList.get(0).add(new DietListAdapter.Data("url","칼륨 듬뿍 고구마죽",350));
+        dataList.get(0).add(new DietListAdapter.Data("url","누룽지 두부 계란죽",314));
+        dataList.get(0).add(new DietListAdapter.Data("url","두부 곤약 비빔밥",289));
+        for(int i = 1; i < 7; i++){
+            dataList.add(new ArrayList<DietListAdapter.Data>());
+            dataList.get(i).add(new DietListAdapter.Data("url","오렌지라시 스시",249));
+            dataList.get(i).add(new DietListAdapter.Data("url","취나물 비빔밥",350));
+            dataList.get(i).add(new DietListAdapter.Data("url","곰취 쌈밥",290));
+        }
+
+        //현재 요일로 default 설정
+        Calendar calendar = Calendar.getInstance();
+        int date = calendar.get(Calendar.DAY_OF_WEEK)-1;
+        dietListAdapter = new DietListAdapter(dataList.get(date));
+        dietRecycler.setAdapter(dietListAdapter);
+        weekAdapter = new CategoryAdapter(weeklist,getContext());
+        weekRecycler.setAdapter(weekAdapter);
+        int prePositon = weekAdapter.getClick_position();
+        weekAdapter.setClick_position(date);
+        weekAdapter.notifyItemChanged(prePositon);
+        weekAdapter.notifyItemChanged(date);
+
+        //요일의 kcal percentage 설정
+        int result = (dietListAdapter.getTotalCalories()*100)/2000;
+        progressBar.setProgress(result);
+        percentage.setText(result + "% (1인 하루 권장량 기준)");
+        totalCalories.setText(dietListAdapter.getTotalCalories() + " kcal");
+
+        //요일을 클릭했을때
+        weekAdapter.setOnItemClickListener(new CategoryAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                int prePositon = weekAdapter.getClick_position();
+                weekAdapter.setClick_position(position);
+                weekAdapter.notifyItemChanged(prePositon);
+                weekAdapter.notifyItemChanged(position);
+                dietListAdapter.changeData(dataList.get(position));
+                int result = (dietListAdapter.getTotalCalories()*100)/2000;
+                progressBar.setProgress(result);
+                percentage.setText(result + "% (1인 하루 권장량 기준)");
+                totalCalories.setText(dietListAdapter.getTotalCalories() + " kcal");
+            }
+        });
     }
 }
---
+++ BillysRecipe/app/src/main/java/kr/ac/ssu/billysrecipe/adapter/DietListAdapter.java
@@ -0,0 +1,126 @@
+package kr.ac.ssu.billysrecipe.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import kr.ac.ssu.billysrecipe.R;
+
+public class DietListAdapter extends RecyclerView.Adapter<DietListAdapter.ViewHolder> {
+    private ArrayList<Data> items;
+
+    public DietListAdapter(ArrayList<Data> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public DietListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.diet_item, parent, false);
+        DietListAdapter.ViewHolder vh = new DietListAdapter.ViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DietListAdapter.ViewHolder holder, int position) {
+        if(position == 0){
+            holder.icon.setImageResource(R.drawable.ic_sunrise);
+            holder.mealtime.setText("아침");
+            holder.view.setVisibility(View.VISIBLE);
+        }
+        else if(position == 1){
+            holder.icon.setImageResource(R.drawable.ic_sun);
+            holder.mealtime.setText("점심");
+            holder.view.setVisibility(View.VISIBLE);
+        }
+        else{
+            holder.icon.setImageResource(R.drawable.ic_moon);
+            holder.mealtime.setText("저녁");
+            holder.view.setVisibility(View.GONE);
+        }
+        //holder.photo.setImageResource();
+        holder.calorie.setText(items.get(position).kcal + "kcal");
+        holder.name.setText(items.get(position).name);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void changeData(ArrayList<DietListAdapter.Data> items){
+        this.items = items;
+        notifyDataSetChanged();
+    }
+
+    public int getTotalCalories(){
+        return items.get(0).kcal + items.get(1).kcal + items.get(2).kcal;
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView icon;
+        TextView mealtime;
+        ImageView photo;
+        TextView name;
+        TextView calorie;
+        View view;
+
+        ViewHolder(View itemView) {
+            super(itemView) ;
+
+            icon = itemView.findViewById(R.id.diet_item_icon);
+            mealtime = itemView.findViewById(R.id.diet_item_mealtime);
+            photo = itemView.findViewById(R.id.diet_item_photo);
+            name = itemView.findViewById(R.id.diet_item_name);
+            calorie = itemView.findViewById(R.id.diet_item_calorie);
+            view = itemView.findViewById(R.id.diet_item_view);
+        }
+    }
+
+    public static class Data{
+        private String photo;   //수정필요
+        private String name;
+        private int kcal;
+
+        public Data(String photo, String name, int kcal) {
+            this.photo = photo;
+            this.name = name;
+            this.kcal = kcal;
+        }
+
+        public Data(){}
+
+        public String getPhoto() {
+            return photo;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getKcal() {
+            return kcal;
+        }
+
+        public void setPhoto(String photo) {
+            this.photo = photo;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setKcal(int kcal) {
+            this.kcal = kcal;
+        }
+    }
+}
